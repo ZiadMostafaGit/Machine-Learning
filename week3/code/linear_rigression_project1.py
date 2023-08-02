f8c9db0c26a22d6666c8bd9d8df57859fc436451
ZiadMostafaGit <github2003@gmail.com>
2023-08-02 14:35:24 +0300

    first linear_regression project

--- week3/code/linear_rigression_project1.py
+++ week3/code/linear_rigression_project1.py
@@ -1,23 +1,55 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sas
+import seaborn as sns
 import sklearn.datasets
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn import metrics
 from scipy import stats
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+
 df = pd.read_csv(
-    "/home/ziad/GitHub/Machine-Learning-journey/week3/data/california_housing_test.csv")
+    "/home/ziad/GitHub/Machine-Learning-journey/week3/data/Housing_Data (2).csv")
 
 df.dropna(inplace=True)
-print(df.shape)
+
+df = df.drop("Address", axis=1)
+
+x = df.drop("Price", axis=1)
+
+y = df["Price"]
+
+scaler = StandardScaler()
+y = y.values.reshape(-1, 1)
+x_norm = pd.DataFrame(scaler.fit_transform(x), columns=x.columns)
+y_norm = pd.DataFrame(scaler.fit_transform(y))
 
 
-z_scores = stats.zscore(df)
+x_norm, x_test, y_norm, y_test = train_test_split(
+    x_norm, y_norm, test_size=0.3)
 
-_scores = stats.zscore(df)
-abs_z_scores = np.abs(z_scores)
-filtered_entries = (abs_z_scores < 3).all(axis=1)
-new_df = df[filtered_entries]
-print(new_df)
+
+modil = LinearRegression()
+
+modil.fit(x_norm, y_norm)
+
+w = modil.coef_
+
+b = modil.intercept_
+prediction = modil.predict(x_test)
+
+
+mae = metrics.mean_absolute_error(y_test, prediction)
+mse = metrics.mean_squared_error(y_test, prediction)
+r2 = metrics.r2_score(y_test, prediction)
+
+# Print the results
+print(f"MAE: {mae:.2f}")
+print(f"MSE: {mse:.2f}")
+print(f"R2 score: {r2:.2f}")
+
+# plt.scatter(y_test, prediction)
+# plt.hist(y_test-prediction)
+# plt.show()
